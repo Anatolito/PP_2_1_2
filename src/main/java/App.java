3f0192d8253dd24e8e28036d5217f65dd27a50c3
anatolito <anatolito@yandex.ru>
2023-10-05 23:03:06 +0300

    Change classes App, AppConfig, add class Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld helloBean = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloBean.getMessage());
+        HelloWorld helloBean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Переменные HelloWorld ссылаются на один объект? " + helloBean.equals(helloBean2));
+        System.out.println("Переменные Cat ссылаются на один объект? " + catBean.equals(catBean2));
     }
 }
